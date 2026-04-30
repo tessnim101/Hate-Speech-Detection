c445072871f18297ffc5475181466948d4ce0392
David Brantschen <david.brantschen@epfl.ch>
2026-04-30 08:31:27 +0200

    write training results to .csv file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from datasets import Dataset
 from transformers import AutoTokenizer
 import pandas as pd
 import os
+from datetime import datetime
 
 from data.loader import load_data
 from data.preprocessing import stratified_sample, ids_to_text
@@ -58,19 +59,26 @@ def format_dataset(dataset):
     dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
     return dataset
 
-def save_results(metrics, filepath="results/results.csv"):
-    df = pd.DataFrame([metrics])
+def save_results(trainer, filepath="results/results.csv", extra_info=None):
+    logs = trainer.state.log_history
+
+    all_logs = [x for x in logs if "loss" in x or "eval_loss" in x]
+    df = pd.DataFrame(all_logs)
+
+    if extra_info is not None:
+        for key, value in extra_info.items():
+            df[key] = value
 
     file_exists = os.path.isfile(filepath)
 
     df.to_csv(
         filepath,
         mode="a",
-        header=not file_exists,  # only write header once
+        header=not file_exists,
         index=False
     )
 
-def run_baseline(df_train, df_test):
+def run_baseline(df_train, df_test, run_id):
     tokenizer = AutoTokenizer.from_pretrained(CONFIG["model_name"])
 
     train_ds = prepare_dataset(df_train, ["text"])
@@ -89,16 +97,17 @@ def run_baseline(df_train, df_test):
     trainer = build_trainer(model, train_ds, test_ds, tokenizer, CONFIG)
     trainer.train()
     
-    metrics = trainer.evaluate()
+    extra_info = {
+        "run_id": run_id,
+        "model": "baseline",
+        "context": False,
+        "max_len": CONFIG["max_len"]
+    }
 
-    metrics["model"] = "baseline"
-    metrics["context"] = False
-    metrics["max_len"] = CONFIG["max_len"]
-
-    save_results(metrics)
+    save_results(trainer, extra_info=extra_info)
 
 
-def run_context(df_train, df_test):
+def run_context(df_train, df_test, run_id):
     tokenizer = AutoTokenizer.from_pretrained(CONFIG["model_name"])
 
     df_train = ids_to_text(df_train)
@@ -120,28 +129,31 @@ def run_context(df_train, df_test):
     trainer = build_trainer(model, train_ds, test_ds, tokenizer, CONFIG)
     trainer.train()
 
-    metrics = trainer.evaluate()
+    extra_info = {
+        "run_id": run_id,
+        "model": "context",
+        "context": True,
+        "max_len": CONFIG["max_len"]
+    }
 
-    metrics["model"] = "context"
-    metrics["context"] = True
-    metrics["max_len"] = CONFIG["max_len"]
-
-    save_results(metrics)
+    save_results(trainer, extra_info=extra_info)
 
 
 if __name__ == "__main__":
+    RUN_ID = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
     path_data = "data/spanish_subset/"
 
     df_train, df_test = load_data(path_data)
 
     # small CPU-friendly subsets
     df_train_small = stratified_sample(df_train, 100)
-    df_test_small = stratified_sample(df_test, 50)
+    df_test_small = stratified_sample(df_test, 30)
 
-    run_baseline(df_train_small, df_test_small)
+    run_baseline(df_train_small, df_test_small, run_id=RUN_ID)
 
     # context experiment
     df_train_small = stratified_sample(df_train, 200)
     df_test_small = stratified_sample(df_test, 50)
 
-    #run_context(df_train_small, df_test_small)
+    #run_context(df_train_small, df_test_small, run_id=RUN_ID)
